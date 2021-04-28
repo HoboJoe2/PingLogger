35c7246b1b39a563acc76f5868604b4ddee30296
Joe Dickson <joedickson4556@gmail.com>
2021-04-28 16:36:22 +1000

    changed ping destination from google.com.au to www.google.com

--- main.bat
+++ main.bat
@@ -6,7 +6,7 @@ echo Command is running. Press CTRL+C to quit.
 FOR /L %%i IN () DO (
 
    set pingreturn=
-   FOR /F "delims=" %%p IN ('ping -n 1 www.google.com.au') DO SET pingreturn=%%p
+   FOR /F "delims=" %%p IN ('ping -n 1 www.google.com') DO SET pingreturn=%%p
 
    set curtime=
    FOR /F "delims=" %%t IN ('powershell -NoLogo -NonInteractive -OutputFormat Text -Command "[DateTime]::Now.ToString(\"yyyy-MM-dd HH:mm:ss\")"') DO SET curtime=%%t
