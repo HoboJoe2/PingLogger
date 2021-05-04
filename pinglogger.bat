b225c374efacdcd5a5a42e5971fbcbc2ef981e7a
Joe Dickson <joedickson4556@gmail.com>
2021-05-04 19:00:05 +1000

    changed name of bat file

---
+++ pinglogger.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Command is running. Press CTRL+C to quit.
+
+FOR /L %%i IN () DO (
+
+   set pingreturn=
+   FOR /F "delims=" %%p IN ('ping -n 1 www.google.com') DO SET pingreturn=%%p
+
+   set curtime=
+   FOR /F "delims=" %%t IN ('powershell -NoLogo -NonInteractive -OutputFormat Text -Command "[DateTime]::Now.ToString(\"yyyy-MM-dd HH:mm:ss\")"') DO SET curtime=%%t
+
+   echo !curtime!: >> ping.log
+   echo !pingreturn! >> ping.log
+
+   timeout 10 /NOBREAK > NUL
+
+)
